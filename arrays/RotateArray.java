cde3db749b723de66043c8e1113207119eebfb8c
snehal779 <badgujarsnehal77@gmail.com>
2026-05-13 20:35:02 +0530

    Add Rotate Array solution

---
+++ arrays/RotateArray.java
@@ -0,0 +1,15 @@
+//leetcode 189: Rotate Array
+//Topic: Array
+//Difficulty: Medium
+class RotateArray {
+    public void rotate(int[] nums, int k) {
+    
+    for(int j=0; j<k;j++){
+        int temp = nums[nums.length-1];
+      for(int i=nums.length-1; i>0; i--){
+        nums[i] = nums[i-1];
+      }
+      nums[0]= temp;
+    }
+    }
+}
